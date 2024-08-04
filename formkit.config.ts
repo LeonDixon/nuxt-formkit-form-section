2c3655e25a59e29bbff09e3cb30d89d7e2646a5e
Leon Dixon <me@leondixon.com>
2024-08-04 22:11:16 +0100

    added formkit pro

--- formkit.config.ts
+++ formkit.config.ts
@@ -2,11 +2,13 @@ import { defaultConfig } from '@formkit/vue'
 import { rootClasses } from './formkit.theme'
 import { formSectionPlugin } from './formkit/plugins/form-section-plugin'
 import { createLocalStoragePlugin } from './formkit/plugins/local-storage-plugin'
+import { createProPlugin, inputs } from '@formkit/pro'
 export default defaultConfig({
   config: {
     rootClasses
   },
   plugins: [
+    createProPlugin('fk-a32f74eaf3', inputs),
     formSectionPlugin(),
     createLocalStoragePlugin({
       overwriteOnCommit: false
